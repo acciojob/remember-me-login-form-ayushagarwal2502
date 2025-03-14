5128a431f2a5a6f86ca4790d0fcc4b3c45291e15
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-03-14 23:12:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 //your JS code here. If required.
-
-
 function submitForm(event){
-	let username=document.getElementById("username").value;
-	let password=document.getElementById("password").value;
-	let isChecked=document.getElementById("checkbox").checked;
-	event.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let isChecked = document.getElementById("checkbox").checked;
+    event.preventDefault();
 
-	if(isChecked){
-		localStorage.setItem("data",{username,password});
-	}
-	alert(`Logged in as ${username}`)
-	
+    if(isChecked){
+        localStorage.setItem("data", JSON.stringify({username, password}));
+    }
+    alert(`Logged in as ${username}`);
 }
