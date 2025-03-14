ab578c827051613e19a0acc41e69899fde6b8c3d
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-03-14 23:08:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+
+function submitForm(event){
+	let username=document.getElementById("username").value;
+	let password=document.getElementById("password").value;
+	let isChecked=document.getElementById("checkbox").checked;
+	event.preventDefault();
+
+	if(isChecked){
+		localStorage.setItem("data",{username,password});
+	}
+	alert(`Logged in as ${username}`)
+	
+}
